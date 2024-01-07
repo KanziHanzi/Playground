e9a69b8a3959fab26ad0b24f59739dc5b20d56aa
Janik Grigutsch <github@janikgrigutsch.com>
2024-01-07 13:37:06 +0100

    chore: inject p5 globally in vite config

--- src/flows/audioVisualizer.ts
+++ src/flows/audioVisualizer.ts
@@ -1,23 +1,21 @@
 import p5 from "p5";
-// import SoundFile from "p5/lib/addons/p5.sound";
+import "p5/lib/addons/p5.sound";
 
 const sketch = (context: p5) => {
-  // console.log(SoundFile);
-
   context.setup = () => {
     context.createCanvas(context.windowWidth, context.windowHeight);
     context.background("#000");
   };
 
   context.draw = () => {
-    context.fill(255, 0, 0);
-    context.noStroke();
-    context.rectMode(context.CENTER);
-    context.rect(context.mouseX, context.mouseY, 50, 50);
+    // context.fill(255, 0, 0);
+    // context.noStroke();
+    // context.rectMode(context.CENTER);
+    // context.rect(context.mouseX, context.mouseY, 50, 50);
   };
 
   context.mousePressed = () => {
-    context.background("#000");
+    // context.background("#000");
   };
 };
 
