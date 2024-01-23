2192e5962871e612454d5bd95f14b5758063cc7c
Janik Grigutsch <github@janikgrigutsch.com>
2024-01-23 09:46:03 +0100

    feat: finish sineWave

--- src/flows/sineWave/sineWave.ts
+++ src/flows/sineWave/sineWave.ts
@@ -1,10 +1,10 @@
 import p5 from "p5";
-import { getRandomStrokeColor } from "src/utils/randomStroke";
+import { getRgbValues } from "./utils";
 
 const rectWidth = 600;
 const rectHeight = 600;
 
-const limit = 200;
+const rectCount = 200;
 
 const sketch = (context: p5) => {
   let width: number;
@@ -24,23 +24,21 @@ const sketch = (context: p5) => {
 
   context.draw = () => {
     context.background(10, 20, 30);
-    context.strokeWeight(2)
+    context.strokeWeight(2);
     context.noFill();
 
     context.translate(width / 2, height / 2);
 
-    for (let i = 0; i <= limit; i++) {
+    for (let i = 0; i <= rectCount; i++) {
       context.push();
 
-      if (i % 5 === 0) {
-        // context.stroke(getRandomStrokeColor());
-      } else {
-        context.stroke("#fff");
-      }
+      const { r, g, b } = getRgbValues(context);
 
-      context.rotate(context.sin(context.frameCount + i * 3) * 100);
+      context.stroke(r, g, b);
 
-      context.rect(0, 0, rectWidth - i * 3, rectHeight - i * 3, limit - i);
+      context.rotate(context.sin(context.frameCount + i * 1.5) * 100);
+
+      context.rect(0, 0, rectWidth - i * 3, rectHeight - i * 3, rectCount - i);
 
       context.pop();
     }
