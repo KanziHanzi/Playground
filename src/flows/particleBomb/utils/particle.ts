0246bd331daa2624dde34b931044b9d4ecac7cd5
Janik Grigutsch <github@janikgrigutsch.com>
2024-01-25 09:33:15 +0100

    feat: add randomness to particle vectors

--- src/flows/particleBomb/utils/particle.ts
+++ src/flows/particleBomb/utils/particle.ts
@@ -3,13 +3,17 @@ import { Particle } from "src/common";
 import { getRandomStrokeColor } from "src/utils/randomStroke";
 
 const accelerationMultiplier = 0.1;
+const randomRange = { x: -10, y: 10 };
 
 export const createParticle = (context: p5InstanceExtensions) => {
   const positionVector = context.createVector(
-    context.winMouseX,
-    context.winMouseY
+    context.winMouseX + context.random(randomRange.x, randomRange.y),
+    context.winMouseY + context.random(randomRange.x, randomRange.y)
   );
-  const velocityVector = context.createVector(0, 0);
+  const velocityVector = context.createVector(
+    context.random(randomRange.x, randomRange.y),
+    context.random(randomRange.x, randomRange.y)
+  );
   const accelerationVector = p5.Vector.random2D()
     .normalize()
     .mult(accelerationMultiplier, accelerationMultiplier);
