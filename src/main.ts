042b2c13db1f514ce6f0f11cc7b2b4d955052cef
Janik Grigutsch <github@janikgrigutsch.com>
2024-01-27 13:17:55 +0100

    feat: first draft

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 // import "src/flows/audioVisualizer";
 // import "src/flows/mouseTrail";
 // import "src/flows/sineWave";
-import "src/flows/particleBomb";
+// import "src/flows/particleBomb";
+import "src/flows/loadingIndicator"
