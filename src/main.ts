becd6488654447111ce896760837e6887d652c08
Janik Grigutsch <github@janikgrigutsch.com>
2024-01-30 09:54:45 +0100

    feat: first draft

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,5 @@
 // import "src/flows/sineWave";
 // import "src/flows/particleBomb";
 // import "src/flows/loadingIndicator"
-import "src/flows/perlinNoiseTrajectory";
+// import "src/flows/perlinNoiseTrajectory";
+import "src/flows/perlinNoiseFlowField";
