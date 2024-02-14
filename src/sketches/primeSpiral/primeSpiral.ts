48bd29e7afbff4cdac9ac85e6a1c3abe46aa1837
Janik Grigutsch <github@janikgrigutsch.com>
2024-02-14 09:57:41 +0100

    feat: spiral draft

--- src/sketches/primeSpiral/primeSpiral.ts
+++ src/sketches/primeSpiral/primeSpiral.ts
@@ -1,13 +1,29 @@
 import p5 from "p5";
+import { isPrimeNumber } from "src/utils";
+
+const maxNumber = 1000;
 
 const sketch = (context: p5) => {
   context.setup = () => {
     context.createCanvas(context.windowWidth, context.windowHeight);
     context.background(10, 20, 30);
+    context.stroke("#fff");
   };
 
   context.draw = () => {
-    // runs every frame, main method to draw and render on the canvas
+    context.translate(context.windowWidth / 2, context.windowHeight / 2);
+
+    for (let i = 1; i <= maxNumber; i++) {
+      if (isPrimeNumber(i)) {
+        const angle = context.random(0, context.TWO_PI);
+        const distance = i;
+
+        const x = distance * context.sin(angle);
+        const y = distance * context.cos(angle);
+
+        context.point(x, y);
+      }
+    }
   };
 };
 
