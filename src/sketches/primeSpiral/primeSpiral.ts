d50afa6f81505f25e3db3ed9296fd34097288b79
Janik Grigutsch <github@janikgrigutsch.com>
2024-02-19 09:45:08 +0100

    feat: increment angle for spiral drawing

--- src/sketches/primeSpiral/primeSpiral.ts
+++ src/sketches/primeSpiral/primeSpiral.ts
@@ -2,12 +2,16 @@ import p5 from "p5";
 import { isPrimeNumber } from "src/utils";
 
 const maxNumber = 1000;
+let angle = 0;
+let scaleFactor = 0.2;
 
 const sketch = (context: p5) => {
   context.setup = () => {
     context.createCanvas(context.windowWidth, context.windowHeight);
     context.background(10, 20, 30);
-    context.stroke("#fff");
+    context.fill("#fefefe");
+    context.noStroke();
+    context.angleMode("degrees");
   };
 
   context.draw = () => {
@@ -15,15 +19,17 @@ const sketch = (context: p5) => {
 
     for (let i = 1; i <= maxNumber; i++) {
       if (isPrimeNumber(i)) {
-        const angle = context.random(0, context.TWO_PI);
         const distance = i;
 
         const x = distance * context.sin(angle);
         const y = distance * context.cos(angle);
 
-        context.point(x, y);
+        context.text(i, x, y);
+        // context.scale(scaleFactor + i);
       }
+      angle++;
     }
+    context.noLoop();
   };
 };
 
