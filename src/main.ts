ba42158d023df3ec76889ab670932a2285a1a3aa
Janik Grigutsch <github@janikgrigutsch.com>
2024-02-27 10:50:02 +0100

    chore: change default sketch

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@
 // import "src/sketches/particleBomb";
 // import "src/sketches/loadingIndicator"
 // import "src/sketches/perlinNoiseTrajectory";
-// import "src/sketches/perlinNoiseFlowField";
-import "src/sketches/primeSpiral"
+import "src/sketches/perlinNoiseFlowField";
+// import "src/sketches/primeSpiral"
