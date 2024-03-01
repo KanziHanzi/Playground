46161f2571731c982c2c2917608cc3e429da938a
Janik Grigutsch <github@janikgrigutsch.com>
2024-03-01 09:48:37 +0100

    feat: draw vector by pixel and collision detection draft

--- src/sketches/shatteredGlas/shatteredGlas.ts
+++ src/sketches/shatteredGlas/shatteredGlas.ts
@@ -1,6 +1,25 @@
-import p5 from "p5";
+import p5, { p5InstanceExtensions } from "p5";
+import { Boundaries } from "src/common";
+
+type Position = {
+  x: number;
+  y: number;
+};
 
 const lineCount = 10;
+
+const paintedPixels: Position[] = [];
+
+const lines: Line[] = [];
+
+let accessIndex = 0;
+
+const boundaries: Boundaries = {
+  x: 600,
+  y: 800,
+  negativeX: 0,
+  negativeY: 0,
+};
 
 const sketch = (context: p5) => {
   context.setup = () => {
@@ -19,25 +38,103 @@ const sketch = (context: p5) => {
       frame.child(canvas);
     }
 
+    for (let i = 0; i < lineCount; i++) {
+      const vector = context.createVector(
+        context.random(0, 600),
+        context.random(0, 800)
+      );
+
+      const line = new Line(context, vector);
+
+      lines.push(line);
+    }
+
     context.background("#FFFFF8");
+    context.stroke("#000");
   };
 
   context.draw = () => {
-    for (let i = 0; i < lineCount; i++) {
-      context.line(
-        context.random(0, 600),
-        0,
-        context.random(0, 600),
-        800
-      );
+    const line = lines[accessIndex];
+
+    if (!line.isOutOfBounds(boundaries)) {
+      line.drawCurrentPixel();
+      line.updatePosition();
+    } else {
+      if (accessIndex === lineCount - 1) return;
+
+      accessIndex++;
     }
 
-    context.noLoop();
-  };
+    if (
+      paintedPixels.find((pixel) => {
+        const { x, y } = line.getPosition();
+        const drawingPosition = { x: Math.floor(x), y: Math.floor(y) };
 
-  context.mouseMoved = () => {
-    console.log(context.mouseX, context.mouseY);
+        const isColliding = drawingPosition.x === pixel.x && drawingPosition.y === pixel.y;
+        if (isColliding) {
+          console.log(drawingPosition, pixel)
+        }
+
+        return isColliding
+      })
+    ) {
+      console.log("collision");
+      if (accessIndex === lineCount - 1) return;
+
+      accessIndex++;
+    }
+
+    const { x, y } = line.getPosition();
+    const flooredPixel = { x: Math.floor(x), y: Math.floor(y) };
+    paintedPixels.push(flooredPixel);
   };
 };
+
+class Line {
+  context: p5InstanceExtensions;
+  position: p5.Vector;
+  velocity: p5.Vector;
+
+  constructor(context: p5InstanceExtensions, position: p5.Vector) {
+    this.context = context;
+    this.position = position;
+
+    const angle = context.random(0, context.TWO_PI);
+    const angleVector = p5.Vector.fromAngle(angle);
+
+    this.velocity = angleVector;
+  }
+
+  public drawCurrentPixel() {
+    const { x, y } = this.position;
+
+    this.context.point(x, y);
+  }
+
+  public updatePosition() {
+    this.position.add(this.velocity);
+  }
+
+  public isOutOfBounds(boundaries: Boundaries): boolean {
+    switch (true) {
+      case this.position.x < boundaries.negativeX:
+        return true;
+      case this.position.x > boundaries.x:
+        return true;
+      case this.position.y < boundaries.negativeY:
+        return true;
+      case this.position.y > boundaries.y:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  public getPosition(): Position {
+    const { x, y } = this.position;
+
+    return { x, y };
+  }
+}
 
 new p5(sketch);
