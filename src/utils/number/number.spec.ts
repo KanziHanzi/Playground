2b6d481aacca661b6ddf61529ca3fb3cdcaef5e2
Janik Grigutsch <github@janikgrigutsch.com>
2024-03-11 09:14:49 +0100

    fix: test prime number testing spec

--- src/utils/number/number.spec.ts
+++ src/utils/number/number.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { isPrimeNumber } from "./number";
 
 test("detects if number is prime", () => {
-  const primeNumber = 12;
+  const primeNumber = 13;
   const nonPrimeNumber = 9;
 
   expect(isPrimeNumber(primeNumber)).toBe(true)
