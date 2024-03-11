f48b620da7566d244594a5801b73cacaeff623e9
Janik Grigutsch <github@janikgrigutsch.com>
2024-03-11 09:13:17 +0100

    feat: add testing job to deploy workflow

--- src/utils/number/number.spec.ts
+++ src/utils/number/number.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { isPrimeNumber } from "./number";
 
 test("detects if number is prime", () => {
-  const primeNumber = 13;
+  const primeNumber = 12;
   const nonPrimeNumber = 9;
 
   expect(isPrimeNumber(primeNumber)).toBe(true)
